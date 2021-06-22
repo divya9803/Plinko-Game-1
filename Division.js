e9ab125330918033014f8e6af1cad043476a8f72
divya9803 <77921863+divya9803@users.noreply.github.com>
2021-06-22 19:02:07 +0530

    Update Division.js

--- Division.js
+++ Division.js
@@ -1,37 +1,23 @@
-class Divisions
+class Division
 {
-    constructor(x,y,width,height) 
-    {
-      var options = {
-                      'isStatic': true,
-                      'density': 1,
-                      'restitution': 0,
-                      'friction': 0
-                    }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
-    }
-    display()
-    {
-      /*push();
-      var pos = this.body.position;
-      rectMode(CENTER);
-      fill("white");
-      rect(pos.x, pos.y, this.width, this.height);
-      pop();*/
+  constructor(x , y , w , h)
+  {
+      var options = 
+      {
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x,y,w,h,options);
+      this.w=w;
+      this.h=h;
+      World.add(world,this.body);
+  }
 
+  display()
+  {
       var pos = this.body.position;
-      //var angle = this.body.angle;
-      
-      push();
-      //translate(pos.x, pos.y);
-      //rotate(angle);
-      stroke(1);
-      fill("white");
       rectMode(CENTER);
-      rect(pos.x, pos.y, this.width, this.height);
-      pop();
-    }
-  };
+      fill("white");
+      rect(pos.x,pos.y,this.w,this.h);
+  }
+};
+

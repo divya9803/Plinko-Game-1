4afc841847bcf925acc3de0801ada7eb2515fc44
divya9803 <77921863+divya9803@users.noreply.github.com>
2021-06-22 19:02:38 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,35 +1,20 @@
-class Ground {
-    constructor(x,y,width,height) {
-      var options = {
-                       'isStatic': true,
-                       'density': 2,
-                       'restitution': 0,
-                       'friction': 1
-                    }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
+class Ground 
+{
+  constructor(x,y,width,height) 
+  {
+    var options = {
+        isStatic: true
     }
-    display()
-    {
-      /*push();
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("white");
-      rect(pos.x, pos.y, this.width, this.height);
-      pop();*/
-
-      var pos = this.body.position;
-      //var angle = this.body.angle;
-      
-      push();
-      //translate(pos.x, pos.y);
-      //rotate(angle);
-      stroke(4);
-      fill("white");
-      rectMode(CENTER);
-      rect(pos.x, pos.y, this.width, this.height);
-      pop();
-    }
-  };
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display()
+  {
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill("white");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};

b7ccb584e3a2ecc12c3d26e6aa0bcfc9c4b044b4
divya9803 <77921863+divya9803@users.noreply.github.com>
2021-06-22 19:03:03 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,29 +1,33 @@
-class Particle
+class Particle 
 {
-    constructor(x,y,radius) 
-    {
-      var options = {
-                       isStatic: false,
-                       restitition: 0.4,
-                       density: 0.5
-                    }
-      this.body = Bodies.circle(x,y,radius,options);
-      this.radius = radius;
-      this.color = color(random(0,255), random(0,255), random(0,255));
+  constructor(x, y,r) 
+  {
+
+      var options =
+      {
+        restitution:0.4
+      }
+      
+      this.r=r;
+      this.body = Bodies.circle(x, y, this.r,options);       
+      this.color=color(random(0, 255), random(0, 255), random(0, 255));
       World.add(world, this.body);
-    }
-    display()
-    {
+
+  }
+  display() 
+  {
       var pos = this.body.position;
       var angle = this.body.angle;
 
       push();
       translate(pos.x, pos.y);
       rotate(angle);
+   
       noStroke();
-      fill(this.color);
+      fill(this.color)
       ellipseMode(RADIUS);
-      ellipse(pos.x, pos.y, this.radius, this.radius);
+      ellipse(0, 0, this.r,this.r);
       pop();
-    }
-  };
+  }
+
+};
